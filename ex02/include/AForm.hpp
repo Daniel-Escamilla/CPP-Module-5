4b27fa66769139d35ee06ceedb02c07ec6f4b7fc
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2025-05-13 01:56:57 +0200

    Refactor AForm class header for improved readability and consistency

--- ex02/include/AForm.hpp
+++ ex02/include/AForm.hpp
@@ -3,18 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   AForm.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: daniel-escamilla <daniel-escamilla@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 17:52:02 by descamil          #+#    #+#             */
-/*   Updated: 2025/05/12 23:47:06 by descamil         ###   ########.fr       */
+/*   Updated: 2025/05/13 01:19:36 by daniel-esca      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef AFORM_HPP
 # define AFORM_HPP
 
-#include <string>
-#include <iostream>
+# include <string>
+# include <iostream>
+# include <fstream>
 
 class Bureaucrat;
 
@@ -26,12 +27,19 @@ class AForm
 		const int			_gradeToExecute;
 		bool				_signed;
 	public:
-		std::string getName() const;
-		bool isSigned() const;
-		int getGradeToSign() const;
-		int getGradeToExecute() const;
+		AForm(std::string name, int SignGrade, int ExecuteGrade);
+		~AForm();
+		AForm(const AForm &copy);
+		AForm &operator=(const AForm &copy);
 
-		void beSigned(const Bureaucrat &bureaucrat);
+		std::string getName() const;
+		bool		isSigned() const;
+		int			getGradeToSign() const;
+		int			getGradeToExecute() const;
+
+		void			beSigned(const Bureaucrat &bureaucrat);
+		void			checkExecutability(Bureaucrat const & executor) const;
+		virtual void	execute(Bureaucrat const & executor) const = 0;
 
 		class GradeTooHighException : public std::exception
 		{
@@ -44,6 +52,27 @@ class AForm
 			public:
 				const char* what() const throw();
 		};
+
+		class FormNotSignedException : public std::exception
+		{
+			public:
+				const char* what() const throw();
+		};
+
+		class GradeTooLowExecuteException : public std::exception
+		{
+			public:
+				const char* what() const throw();
+		};
+
+		class GradeTooHighExecuteException : public std::exception
+		{
+			public:
+				const char* what() const throw();
+		};
+		
 };
+
+std::ostream &operator<<(std::ostream &out, const AForm &form);
 
 #endif
