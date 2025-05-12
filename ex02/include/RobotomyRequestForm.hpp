6c1966e1eb05d9c8a45904318020a765d5ebf587
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2025-05-13 01:57:09 +0200

    Add RobotomyRequestForm class header for form implementation

---
+++ ex02/include/RobotomyRequestForm.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RobotomyRequestForm.hpp                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daniel-escamilla <daniel-escamilla@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 00:26:50 by daniel-esca       #+#    #+#             */
+/*   Updated: 2025/05/13 01:19:31 by daniel-esca      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ROBOTOMYREQUESTFORM_HPP
+# define ROBOTOMYREQUESTFORM_HPP
+
+# include "AForm.hpp"
+# include <cstdlib>
+# include <ctime>
+
+class RobotomyRequestForm : public AForm
+{
+	private:
+		std::string	_target;
+	public:
+		RobotomyRequestForm(std::string target);
+		~RobotomyRequestForm();
+		RobotomyRequestForm(const RobotomyRequestForm &copy);
+		RobotomyRequestForm &operator=(const RobotomyRequestForm &copy);
+
+		void	execute(Bureaucrat const & executor) const;
+};
+
+#endif
