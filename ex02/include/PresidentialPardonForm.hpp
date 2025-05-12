b9416b33c7ec088089f9a65657da9417878f4ac5
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2025-05-13 01:57:05 +0200

    Add PresidentialPardonForm class header for form implementation

---
+++ ex02/include/PresidentialPardonForm.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PresidentialPardonForm.hpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daniel-escamilla <daniel-escamilla@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 00:26:05 by daniel-esca       #+#    #+#             */
+/*   Updated: 2025/05/13 01:19:43 by daniel-esca      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRESIDENTIALPARDONFORM_HPP
+# define PRESIDENTIALPARDONFORM_HPP
+
+# include "AForm.hpp"
+
+class PresidentialPardonForm : public AForm
+{
+	private:
+		std::string	_target;
+	public:
+		PresidentialPardonForm(std::string target);
+		~PresidentialPardonForm();
+		PresidentialPardonForm(const PresidentialPardonForm &copy);
+		PresidentialPardonForm &operator=(const PresidentialPardonForm &copy);
+
+		void	execute(Bureaucrat const & executor) const;
+};
+
+#endif
